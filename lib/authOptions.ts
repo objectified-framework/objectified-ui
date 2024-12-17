e9b690cf0e6025c16bb04af18e0ec8a2c7e2054e
Ken Suenobu <ksuenobu@fastmail.com>
2024-12-16 22:46:35 -0700

    fix: updated to add callbacks for all types of login actions

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -13,5 +13,23 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     })
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log(`signIn: user=${JSON.stringify(user)} account=${JSON.stringify(account)} profile=${JSON.stringify(profile)} email=${JSON.stringify(email)} credentials=${JSON.stringify(credentials)}`);
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      console.log(`redirect: url=${JSON.stringify(url)} baseUrl=${JSON.stringify(baseUrl)}`);
+      return baseUrl;
+    },
+    async session({ session, user, token }) {
+      console.log(`session: session=${JSON.stringify(session)} user=${JSON.stringify(user)} token=${JSON.stringify(token)}`);
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      console.log(`jwt: token=${JSON.stringify(token)} user=${JSON.stringify(user)} account=${JSON.stringify(account)} profile=${JSON.stringify(profile)} isNewUser=${JSON.stringify(isNewUser)}`);
+      return token;
+    }
+  }
 };
 
